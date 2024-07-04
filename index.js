21706aa157c207c7860a2a760fc5dc59e97f4da3
gael terceiro <gb.lmm11@gmail.com>
2024-07-04 10:45:30 -0300

    adicionei funcao soma-sub

---
+++ index.js
@@ -0,0 +1,57 @@
+/* var conteudo  = 'x'
+function funcaoQueImprime(conteudo){
+    console.log(conteudo)
+}
+
+
+function queSoma10EImprime(valor){
+    console.log(valor+10)
+}
+
+function funcaoQueImprime(conteudo){
+    console.log(conteudo)
+    queNaoFazNada()
+    queSoma10EImprime(30)
+}
+
+function queNaoFazNada(){
+    queSoma10EImprime(-1)
+} */
+
+//calculadora
+ function soma (a,b){
+    return a + b;
+ }
+
+ function subtracao (a,b){
+    return a - b;
+ }
+
+ function exibirSoma (){
+    console.log ('chamei o exibir soma')
+    var a = document.getElementById('valorA').value
+    console.log(parseInt(a))
+
+    var b = document.getElementById('valorB').value
+    console.log(parseInt(b))
+
+    var aNumber = parseInt (a)
+    var bNumber = parseInt (b)
+    
+    console.log(soma(aNumber, bNumber))
+ }
+
+ function sub(){
+    console.log('chamei o exibir soma')
+    var a = document.getElementById('valorA').value
+    console.log(parseInt(a))
+
+    var b = document.getElementById('valorB').value
+    console.log(parseInt(b))
+
+    var aNumber = parseInt(a)
+    var bNumber = parseInt (b)
+
+    console.log(subtracao (aNumber, bNumber))
+ }
+   

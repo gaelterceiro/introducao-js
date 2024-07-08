bed087d8069838f75055415dec2e2c871a1bf1f0
gael terceiro <gb.lmm11@gmail.com>
2024-07-08 10:59:59 -0300

    Manipulacao de variaveis

---
+++ STR/index.js
@@ -0,0 +1,102 @@
+/* var a = 'allan'
+console.log(a)
+console.log('chuchu'.toUpperCase())
+console.log('abc'.toLowerCase())
+console.log('casa'.length)
+console.log('cachorro'.length)
+console.log(('casa' + 'casa').length)
+console.log('abc'.charAt(2))
+console.log('abcd'.indexOf('bc')) 
+let frase= "Mergulhando em tecnologia."
+console.log(frase.substring(0,1))
+console.log(frase.substring(3,1)) //erro
+
+let comunicacao= "Olá, sr. nome_user, nome_user informamos que a partir..."
+console.log(comunicacao.replaceAll("nome_user","André"));
+
+var chuchu1 = 'chuchu1'
+var chuchu2 = 'chuchu2'
+
+//console.log(chuchu1+'chuchu2')
+
+var login = "   andre@ema     ilteste.com      "
+console.log(login.trim())
+console.log(login.replaceAll(' ', ''))
+console.log(login.substring(3,12)+login.substring(18,29))
+
+
+let linguaguens = "JS; Java; C#; PHP; Python; Go; Vb; SQL";
+let arrayLinguagens = linguagens.split(";\0; // ['JavaScript', 'Java', C#...] -utilizado para separar
+console.log(arrayLinguagens[4])
+console.log(arrayLinguagens.join('-').toLowerCase()) 
+
+var nomeCompleto = ' allAn Arauj o'
+
+function primeiroNome(canela) {
+    var a = canela.trim().toLowerCase().substring(0,5)
+    return a.charAt(0).toUpperCase() +a.substring(1)
+}
+
+function segundoNome(cachorro){
+    var b = cachorro.trim().substring(6)
+    return b.replaceAll(' ', '')
+
+}
+
+var nome = "Cauan"
+
+console.log("Seu nome é", nome, "e você tem", 23, "anos", "e você é", true)
+
+//template string
+
+console.log(`Seu nome é ${nome} e você tem ${23} anos e você é ${true}`)
+
+
+//Exercício 
+
+var nome = "Gael"
+var idade = 28
+var time = "Fortaleza"
+var cidade = "Fortaleza"
+
+console.log(`Seu nome é ${nome} e você tem ${idade}, torce para o ${time} e mora em ${cidade}`) 
+
+var nome = 28 
+console.log(typeof nome) 
+
+// Tipos de dados em JavaScript
+
+// Number ()
+
+var num1 = 42;
+var num2 = 42.42
+
+console.log(typeof num1);
+console.log(typeof num2);
+
+// NaN 
+
+var num3 = 42 / "dois";
+console.log(num3);
+
+var num4 = "dois"/ 42;
+console.log(num4);
+
+
+
+// Math 
+
+var num5 = Math.PI
+console.log(num5);
+
+var num6 = Math.round(4.7); 
+console.log(num6);
+
+varnum7 = Math.pow(8,2)
+console.log(num7);
+
+var num8 = Math.sqrt(64);
+console.log(num8);
+
+*/
+
